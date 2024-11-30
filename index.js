e1a0cf0e83c1cb78f5b2c7439f23181b99533d7f
Kelsey Hill <kelseykazoo@yahoo.com>
2024-11-30 17:02:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -49,5 +49,7 @@ app.delete("/tasks/:id", async (req, res) => {
 });
 
 // Start Server
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
